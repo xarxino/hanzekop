caa7b6c0e93b0b1289a83d1fd5f6d1194e94b792
Tom Graafmans <graafmans.tom@gmail.com>
2023-12-24 21:12:16 +0100

    Add new files and update configurations

---
+++ src/lib/consts.ts
@@ -0,0 +1,5 @@
+export const SITE_LOCALE = "en";
+export const SITE_TITLE = "Hanzekop";
+export const SITE_URL = "https://hanzekop.com";
+export const SITE_DESCRIPTION =
+  "Hanzekop Holiday Homes is a holiday home rental provider in Stavoren, the Netherlands.";
---
+++ tailwind.config.mjs
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#fdf7e9",
+          100: "#faedc7",
+          200: "#f7d991",
+          300: "#f0b33c",
+          400: "#eca023",
+          500: "#dc8816",
+          600: "#be6710",
+          700: "#974811",
+          800: "#7e3a15",
+          900: "#6b3018",
+          950: "#3e170a",
+        },
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        "spin-veryslow": "spin 15s linear infinite",
+      },
+    },
+    fontFamily: {
+      sans: ['"Noto Sans"', "sans-serif"],
+      serif: ['"Noto Serif Display"', "sans-serif"],
+    },
+  },
+  plugins: [
+    function ({ addComponents, addUtilities, theme }) {
+      addComponents({
+        ".container": {
+          width: "90%",
+          maxWidth: "80rem",
+          margin: "0 auto",
+        },
+      });
+    },
+  ],
+};
